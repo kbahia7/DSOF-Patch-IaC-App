6881887dc2ad5d0105325553727eb9b27a791bf2
kbahia-hs <kenneth.bahia@handshakes.com.sg>
2023-10-27 23:55:28 +0800

    fix

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -18,7 +18,7 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.insecure-bucket.id
   versioning_configuration {
     status = "Enabled"
-    MfaDelete = true
+    MfaDelete = "Enabled"
   }
 }
 
