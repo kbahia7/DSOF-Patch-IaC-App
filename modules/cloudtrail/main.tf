44f229ab8f0f27c3a67c01a72a90c5678e6ecb3f
kbahia-hs <kenneth.bahia@handshakes.com.sg>
2023-10-27 23:41:49 +0800

    fix

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -3,4 +3,6 @@ resource "aws_cloudtrail" "insecure-logging" {
   s3_bucket_name = "my-cloudtrail-bucket"
   enable_log_file_validation = true
   kms_key_id = "a"
+  is_multi_region_trail = true
+  cloud_watch_logs_group_arn = ""
 }
